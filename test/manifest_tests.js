96ef8d41e50c68fba6a878e8042b981c1ec72c82
Ethan Williams <E.Williams@nib.com.au>
2015-10-26 16:23:02 +1100

    import local files from manifest

--- test/manifest_tests.js
+++ test/manifest_tests.js
@@ -25,28 +25,3 @@ describe('manifest npm', function(){
   });
 
 });
-
-describe('manifest npm', function(){
-
-  var result;
-
-  it('should append an npm dependency', function(){
-    result = manifest.toSassString({
-      dependencies: {
-        foo: '^0.0.1'
-      }
-    });
-    assert.equal(result, '@import "foo"');
-  });
-
-  it('should append multiple npm dependencies', function(){
-    result = manifest.toSassString({
-      dependencies: {
-        foo: '^0.0.1',
-        bar: '~0.0.1'
-      }
-    });
-    assert.equal(result, '@import "foo"\n@import "bar"');
-  });
-
-});
